e1298b4724f45e7c6ddf099311d5144e17f18066
A. Karl Kornel <akkornel@stanford.edu>
2023-11-25 20:11:18 -0800

    A script to create a SAML certificate
    This writes an X.509 certificate outo to `cert-XXX.pem` and a 2048-bit
    RSA private key out to `key-XXX.pem`, both in the local directory.
    `XXX` is replaced by some random number.  The cert is valid for 729
    days.
    
    The certificate is very basic, providing only enough information to get
    a SAML IdP to accept it.  The DN is set to bookstack, Stanford University,
    Stanford, California, US.  Our srcc-support email is used as the email.
    The only extensions are the `CA:false` basic constraint, and the
    `serverAuth` extended key usage.
    
    The script also creates a CSR and OpenSSL config file during its work,
    but these are deleted.

---
+++ create-saml-cert.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# This generates a self-signed certificate that is suitable for use in a SAML
+# SP.  It uses a 2048-bit RSA private key, and the certificate has a lifetime
+# of 2 years minus 1 day.
+#
+# The key and cert should be put into Vault, so that they can be loaded when
+# Bookstack starts.  Once Bookstack is running in SAML mode, you can get the
+# SAML SP metadata (which will include this cert), to load into the IdP.
+#
+# NOTE: The SP metadata has its own expiration date.  You'll need to ensure
+# that expiration date is updated.
+
+
+# To ensure we don't overwrite files, include a random number in the filenames.
+RANDOM_NUMBER=${RANDOM}
+OPENSSL_CONFIG="openssl-${RANDOM_NUMBER}.cnf"
+CSR_FILE="csr-${RANDOM_NUMBER}.pem"
+KEY_FILE="key-${RANDOM_NUMBER}.pem"
+CERT_FILE="cert-${RANDOM_NUMBER}.pem"
+
+# Do the work!
+echo "Configuring OpenSSL..."
+cat - >${OPENSSL_CONFIG} <<EOF
+[ req ]
+prompt              = no
+days                = 729
+distinguished_name  = req_dn
+req_extensions      = req_ext
+
+[ req_dn ]
+countryName = US
+stateOrProvinceName = California
+localityName        = Stanford
+organizationName    = Stanford University
+commonName          = bookstack
+emailAddress        = srcc-support@stanford.edu
+
+[ req_ext ]
+basicConstraints    = CA:false
+extendedKeyUsage    = serverAuth
+EOF
+
+echo "Creating Key, Certificate Request, and Certificate..."
+openssl req -config ${OPENSSL_CONFIG} -new -newkey rsa:2048 -nodes -outform PEM --keyout ${KEY_FILE} -out ${CSR_FILE}
+openssl x509 -req -in ${CSR_FILE} -key ${KEY_FILE} -inform PEM -days 729 -out ${CERT_FILE} -outform PEM
+EXPIRATION_DATE=$(openssl x509 -in ${CERT_FILE} -noout -enddate | cut -d= -f2)
+
+echo "Cleaning up..."
+rm -f ${OPENSSL_CONFIG} ${CSR_FILE}
+
+echo "All done!"
+echo
+echo "The self-signed certificate is at ${CERT_FILE}"
+echo "The private key is at ${KEY_FILE}"
+echo
+echo "To write this out to Vault, try something like this:"
+echo "  vault kv put /secret/projects/uit-rc-bookstack/saml key=@${KEY_FILE} cert=@${CERT_FILE}"
+echo
+echo "NOTE: The certificate's expiration date is ${EXPIRATION_DATE}."
+echo "Make sure your metadata expires around this time."
+exit 0
